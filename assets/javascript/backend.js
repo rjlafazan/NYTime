1cc4983eabd715721229af52e7bbfa9c6df74efa
Ryan <rjlafazan@gmail.com>
2018-02-22 21:38:07 -0800

    ajax issue

--- assets/javascript/backend.js
+++ assets/javascript/backend.js
@@ -1,18 +1,42 @@
 
+var searchTerms = $(this).attr('#searchTerm');
+var startYear = $(this).attr('#startYear');
+var endYear = $(this).attr('#endYear');
+
+// var searchTerms = "obama";
+// var startYear = "19911211";
+// var endYear = "19921211";
+// var recordsNumber = 1;
 
 
+var url = "https://api.nytimes.com/svc/search/v2/articlesearch.json?api-key=3e043087ee1a444fb64f1bff59901b08";
 
-var url = "https://api.nytimes.com/svc/search/v2/articlesearch.json";
-url += '?' + $.param({
-  'api-key': "3e043087ee1a444fb64f1bff59901b08"
-  'q': "cats"
-});
+url += '&q=' + searchTerms;
+
+if (startYear !== ""){
+	url += '&begin_date=' + startYear;
+} else if (endYear !== ""){
+	url += '&end_date=' + endYear;
+};
+
+console.log(url);
+
+// url += '?' + $.param({
+// 	'api-key': "3e043087ee1a444fb64f1bff59901b08",
+// 	'q': searchTerms,
+// 	'begin_date': startYear,
+// 	'end_date': endYear
+// });
 
 $.ajax({
-  url: url,
-  method: 'GET',
-}).done(function(result) {
-  console.log(result);
-}).fail(function(err) {
-  throw err;
+	url: url,
+	method: 'GET',
+}).then(function(result) {
+	console.log(result);
+	
+	for (var i = 0; i < result.length; i++) {
+		
+		$('#results').append(result.response.docs[i]);
+	};
+
 });
