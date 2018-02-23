bd1f97bf4387ecd2447d8108d170b6a0607508bf
Ryan <rjlafazan@gmail.com>
2018-02-22 20:18:30 -0800

    move file

--- assets/javascript/backend.js
+++ assets/javascript/backend.js
@@ -1,7 +1,13 @@
+
+
+
+
 var url = "https://api.nytimes.com/svc/search/v2/articlesearch.json";
 url += '?' + $.param({
   'api-key': "3e043087ee1a444fb64f1bff59901b08"
+  'q': "cats"
 });
+
 $.ajax({
   url: url,
   method: 'GET',
