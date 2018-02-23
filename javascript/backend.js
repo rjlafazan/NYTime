8a967d902dd1351087fe108e450fb4e99e938993
Ryan <rjlafazan@gmail.com>
2018-02-22 20:14:12 -0800

    basic javascript

---
+++ javascript/backend.js
@@ -0,0 +1,12 @@
+var url = "https://api.nytimes.com/svc/search/v2/articlesearch.json";
+url += '?' + $.param({
+  'api-key': "3e043087ee1a444fb64f1bff59901b08"
+});
+$.ajax({
+  url: url,
+  method: 'GET',
+}).done(function(result) {
+  console.log(result);
+}).fail(function(err) {
+  throw err;
+});
