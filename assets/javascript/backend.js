6e5459b8874c33db104e2413df2fbb8f27ed9112
morgan crosta <morgancrosta@gmail.com>
2018-02-24 15:23:17 -0800

    append articles doesn't work

--- assets/javascript/backend.js
+++ assets/javascript/backend.js
@@ -1,42 +1,46 @@
+$("#searchButton").on('click', function(event) {
+    event.preventDefault();
+    getArticles();
+})
 
-var searchTerms = $(this).attr('#searchTerm');
-var startYear = $(this).attr('#startYear');
-var endYear = $(this).attr('#endYear');
+function getArticles() {
 
-// var searchTerms = "obama";
-// var startYear = "19911211";
-// var endYear = "19921211";
-// var recordsNumber = 1;
+    var searchTerms = $("#searchTerm").val();
+    var startYear = $("#startYear").val();
+    var endYear = $("#endYear").val();
 
 
-var url = "https://api.nytimes.com/svc/search/v2/articlesearch.json?api-key=3e043087ee1a444fb64f1bff59901b08";
+    var url = "https://api.nytimes.com/svc/search/v2/articlesearch.json?api-key=3e043087ee1a444fb64f1bff59901b08";
 
-url += '&q=' + searchTerms;
+    url += '&q=' + searchTerms;
 
-if (startYear !== ""){
-	url += '&begin_date=' + startYear;
-} else if (endYear !== ""){
-	url += '&end_date=' + endYear;
-};
+    if (startYear !== "") {
+        url += '&begin_date=' + startYear;
+    } else if (endYear !== "") {
+        url += '&end_date=' + endYear;
+    };
 
-console.log(url);
+    console.log(url);
 
-// url += '?' + $.param({
-// 	'api-key': "3e043087ee1a444fb64f1bff59901b08",
-// 	'q': searchTerms,
-// 	'begin_date': startYear,
-// 	'end_date': endYear
-// });
+    // url += '?' + $.param({
+    // 	'api-key': "3e043087ee1a444fb64f1bff59901b08",
+    // 	'q': searchTerms,
+    // 	'begin_date': startYear,
+    // 	'end_date': endYear
+    // });
 
-$.ajax({
-	url: url,
-	method: 'GET',
-}).then(function(result) {
-	console.log(result);
-	
-	for (var i = 0; i < result.length; i++) {
-		
-		$('#results').append(result.response.docs[i]);
-	};
+    $.ajax({
+        url: url,
+        method: 'GET',
+    }).then(function(result) {
+        console.log(result);
 
-});
+        var results = result.response.docs
+
+        for (var i = 0; i < results.length; i++) {
+
+            $('#results').append(results[i]);
+        };
+
+    });
+}
